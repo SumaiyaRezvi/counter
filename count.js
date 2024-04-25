65a80a577074fce71bd0dacd34064198d1c706aa
SumaiyaRezvi <158799562+SumaiyaRezvi@users.noreply.github.com>
2024-04-25 11:43:55 +0600

    Add files via upload

---
+++ count.js
@@ -0,0 +1,21 @@
+
+let x = 1;
+
+function cd(){
+    x++;
+document.getElementById('counter_value').innerHTML = x;
+
+}
+
+function ab(){
+    x--;
+    document.getElementById('counter_value').innerHTML = x;
+   
+    }
+
+    function bc(){
+        x = 1;
+        document.getElementById('counter_value').innerHTML = x;
+       
+        }
+
